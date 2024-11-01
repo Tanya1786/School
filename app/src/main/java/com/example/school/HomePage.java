c5b736cb1a872e0c7e896bc0a7d536da45e3bc90
George <georgejw555@gmail.com>
2024-11-01 16:50:38 -0700

    Added page links for all buttons - GW

--- app/src/main/java/com/example/school/HomePage.java
+++ app/src/main/java/com/example/school/HomePage.java
@@ -1,6 +1,9 @@
 package com.example.school;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HomePage extends AppCompatActivity {
+    //schedule button, find student button, evaluation button & logout button
+    Button btnSchedule, btnStud, btnEval, btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,43 @@ public class HomePage extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnSchedule = findViewById(R.id.btnSchedule);
+        btnStud = findViewById(R.id.btnStud);
+        btnEval = findViewById(R.id.btnEval);
+        btnLogout = findViewById(R.id.btnLogout);
+
+        btnSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, SchedulePage.class);
+                startActivity(intent);
+            }
+        });
+
+        btnStud.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, FindStudentPage.class);
+                startActivity(intent);
+            }
+        });
+
+        btnEval.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, EvaluationsPage.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
