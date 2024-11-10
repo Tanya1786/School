458ab5440436eda5b46b065c1eebf21e39d7fd1a
George <georgejw555@gmail.com>
2024-11-10 11:17:49 -0800

    Simplified HomePage title & added GoBack button to SchedulePage - GW

--- app/src/main/java/com/example/school/SchedulePage.java
+++ app/src/main/java/com/example/school/SchedulePage.java
@@ -1,6 +1,9 @@
 package com.example.school;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SchedulePage extends AppCompatActivity {
+    Button btnGoBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,16 @@ public class SchedulePage extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnGoBack = findViewById(R.id.btnGoBack);
+
+        btnGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SchedulePage.this, HomePage.class);
+                startActivity(intent);
+            }
         });
     }
 }
