5eb9bbc736ba753500dc0e5a6989001a1ab78f4c
George <georgejw555@gmail.com>
2024-11-10 23:16:18 -0800

    Also added code to FindStudentPage.java - GW

--- app/src/main/java/com/example/school/FindStudentPage.java
+++ app/src/main/java/com/example/school/FindStudentPage.java
@@ -1,6 +1,8 @@
 package com.example.school;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class FindStudentPage extends AppCompatActivity {
+    Spinner spClasses, spStudents;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,18 @@ public class FindStudentPage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        spClasses = findViewById(R.id.spClasses);
+        spStudents = findViewById(R.id.spStudents);
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+//                this,
+//                R.array.divisions,
+//                android.R.layout.simple_spinner_item
+//        );
+//        // Specify the layout to use when the list of choices appears.
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        // Apply the adapter to the spinner.
+//        spClasses.setAdapter(adapter);
     }
 }
